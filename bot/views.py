395453ab5fa3faf9537cf74c4be790062bded668
shumilov <wumilovsergey@gmail.com>
2024-02-07 17:52:39 +0300

    message class

--- bot/views.py
+++ bot/views.py
@@ -5,74 +5,73 @@ from rest_framework.response import Response
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 from user_profile.models import Profile
+from .serializers import telegram_format
+
+import json
+
+KEYMOARD = {
+        "inline_keyboard" :  [
+            [
+                {'text': '◀', 'callback_data': json.dumps({"menu":"menu"})}
+            ]
+        ]
+}
 
 @api_view(['POST'])
 def getMessage(request):
-    json = flatten_json(request.data)
-    if "message.text" in json:
-        text = json["message.text"]
-        if "/start" in text and len(text) > 8:
-            key = text.replace("/start ", "")
-            chat_id = json["message.from.id"]
-            info = user_info(json)
+    message = telegram_format(request.data)
+    
+    # print(r)
 
-# IS USER EXIST?
-            try:
-                user = User.objects.get(username=chat_id)
-            except:
-                user = ""
 
-# DELETE OLD TOKEN OF EXIST USER
-            try:  
-                token = Token.objects.get(user=user)
-                token.delete()
-            except:
-                pass
 
-# REFRASH NEW TOKEN OF EXIST USER
-            try:
-                token = Token.objects.get(key=key)
-                old_user = token.user
-                token.user = user
-                profile = Profile.objects.get_or_create(user=user)
-                token.save()
-                old_user.delete()
-            except:
-                pass     
-# CTEATE NEW USER AND SET TOKEN
-            try:
-                token = Token.objects.get(key=key)
-                user = token.user
-                user.username = chat_id
-                profile = Profile.objects.get_or_create(user=user)
-                user.save()
-            except:
-                # BAD TOKEN BUT EXIST USER
-                token = Token.objects.create(user=user)
+
+
+
+#     json = flatten_json(request.data)
+#     if "message.text" in json:
+#         text = json["message.text"]
+#         if "/start" in text and len(text) > 8:
+#             key = text.replace("/start ", "")
+#             chat_id = json["message.from.id"]
+#             info = user_info(json)
+
+# # IS USER EXIST?
+#             try:
+#                 user = User.objects.get(username=chat_id)
+#             except:
+#                 user = ""
+
+# # DELETE OLD TOKEN OF EXIST USER
+#             try:  
+#                 token = Token.objects.get(user=user)
+#                 token.delete()
+#             except:
+#                 pass
+
+# # REFRASH NEW TOKEN OF EXIST USER
+#             try:
+#                 token = Token.objects.get(key=key)
+#                 old_user = token.user
+#                 token.user = user
+#                 profile = Profile.objects.get_or_create(user=user)
+#                 token.save()
+#                 old_user.delete()
+#             except:
+#                 pass     
+# # CTEATE NEW USER AND SET TOKEN
+#             try:
+#                 token = Token.objects.get(key=key)
+#                 user = token.user
+#                 user.username = chat_id
+#                 profile = Profile.objects.get_or_create(user=user)
+#                 user.save()
+#             except:
+#                 # BAD TOKEN BUT EXIST USER
+#                 token = Token.objects.create(user=user)
                 
-    return Response()
+    return Response("ok", status=200)
 # 
 # 
 # https://t.me/wget_bash_bot?start=81479fd7094e7adade40261a8eda663769efbb8f
 
-def flatten_json(json_obj, prefix=''):
-    flat_json = {}
-    for key, value in json_obj.items():
-        new_key = f"{prefix}{key}"
-        if isinstance(value, dict):
-            flat_json.update(flatten_json(value, f"{new_key}."))
-        else:
-            flat_json[new_key] = value
-    return flat_json
-
-def user_info(json):
-    if "message.from.first_name" in json:
-        user_info = json["message.from.first_name"]
-    elif "message.from.last_name" in json:
-        user_info = json["message.from.last_name"]
-    elif "message.from.username" in json:
-        user_info = json["message.from.username"]
-
-    else:
-        user_info = "anonym user"
-    return user_info
